acd16c443b667f1121b0983f7879a049f49d6906
YeFeng233 <YeFeng233@users.noreply.github.com>
2026-09-17 04:41:09 +0800

    release: bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.yefeng.majmax"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
 
         ndk {
             abiFilters += shippedAbis
