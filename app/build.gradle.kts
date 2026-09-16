fe82c64241994f40ddae7b8559ca451058f76eeb
YeFeng233 <YeFeng233@users.noreply.github.com>
2026-09-17 02:20:26 +0800

    release: prepare version 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.yefeng.majmax"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         ndk {
             abiFilters += shippedAbis
